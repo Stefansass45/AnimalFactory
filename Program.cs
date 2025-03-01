50ab62a92fa877790b3e9505575907f88c861b65
Stefansass45 <126760584+Stefansass45@users.noreply.github.com>
2025-03-01 15:15:57 +0200

    factory code with diagram

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace animalFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalFactory factory = new AnimalFactory();
+            Console.WriteLine("please enter the species type");
+            String AnimalSelected = Console.ReadLine();
+            IAnimal animal = factory.GetAnimal(AnimalSelected);
+            Console.WriteLine("Common name: {0} Genus: {1} Species: {2}",
+                animal.getCommonName(), animal.getGenus(), animal.getSpecies());
+        }
+    }
+}
---
+++ Wolf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animalFactory
+{
+    internal class Wolf : IAnimal
+    {
+        public string getCommonName()
+        {
+            return "Wolf";
+        }
+
+        public string getGenus()
+        {
+            return "Canis";
+        }
+
+        public string getSpecies()
+        {
+            return "Lupis";
+        }
+    }
+}
